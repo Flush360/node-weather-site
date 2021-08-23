f579b9b81e9cc9bb86b28a45ac4cd2df26f2acdc
Flush360 <iskra362@gmail.com>
2021-08-23 17:04:22 +0600

    Fixed fort and fetch

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const geocode = require("./utils/geocode");
 const forecast = require("./utils/forecast");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 // paths for express
 const viewsPath = path.join(__dirname, "../templates/views");
 const partialsPath = path.join(__dirname, "../templates/partials");
